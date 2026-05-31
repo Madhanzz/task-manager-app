b63afe79c87f00831426636de6f80e6099896819
Madhanzz <madhankumar6203@gmail.com>
2026-05-31 18:48:55 +0530

    Setup Express server1

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const User = require("./models/User");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -12,7 +20,8 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
